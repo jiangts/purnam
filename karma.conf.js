949b354a461fc4e723a8613e98d7b0886a63286c
Chris Zheng <z@caudate.me>
2013-10-29 23:00:00 +1100

    starting to look at it again

--- karma.conf.js
+++ karma.conf.js
@@ -1,53 +1,70 @@
 // Karma configuration
+// Generated on Tue Oct 29 2013 22:49:41 GMT+1100 (EST)
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(config) {
+  config.set({
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,  
-  'harness/app/libs/angular/angular.js',
-  'harness/app/libs/angular/angular-mocks.js',
-  'harness/unit/*.js',
-];
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
-// list of files to exclude
-exclude = [];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-// web server port
-port = 8080;
 
-// cli runner port
-runnerPort = 9100;
+    // list of files / patterns to load in the browser
+    files: [
+    'harness/app/libs/angular/angular.js',
+    'harness/app/libs/angular/angular-mocks.js',
+    'harness/unit/*.js'
+    ],
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Firefox'];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
--- project.clj
+++ project.clj
@@ -1,38 +1,33 @@
-(defproject purnam "0.1.1-SNAPSHOT"
+(defproject purnam "0.1.0"
   :description "A better javascript experience on clojurescript"
   :url "http://www.github.com/zcaudate/purnam"
   :license {:name "The MIT License"
             :url "http://opensource.org/licencses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1835"]
-                 ;;;[purnam/purnam-js      "0.1.1-SNAPSHOT"]
-                 ;;[purnam/purnam-angular "0.1.1-SNAPSHOT"]
-                 ]
-  :sub 
-    ["purnam-js"
-     "purnam-angular"]
+                 [org.clojure/clojurescript "0.0-1978"]
+                 [purnam/purnam-js      "0.1.0"]
+                 [purnam/purnam-angular "0.1.0"]]
+  :sub ["purnam-js"
+        "purnam-angular"]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
   :plugins [[lein-sub "0.2.1"]
-            [lein-cljsbuild "0.3.0"]
+            [lein-cljsbuild "0.3.4"]
             [lein-midje "3.0.1"]]
   :test-paths ["purnam-js/src" "purnam-js/test" "purnam-angular/src" "purnam-angular/test"]
   :cljsbuild
   {:builds
    [{:source-paths ["purnam-js/src" "purnam-angular/src" "test"],
      :id "js-test",
-     :compiler
-     {:pretty-print true,
-      :output-to "harness/unit/purnam-js-unit.js",
-      :optimizations :whitespace}}
-      {:source-paths ["purnam-js/src" "demos/crafty"],
-       :id "crafty-demo",
-       :compiler
-       {:pretty-print true,
-        :output-to "harness/app/scripts/crafty-demo.js",
-        :optimizations :whitespace}}
+     :compiler {:pretty-print true,
+                :output-to "harness/unit/purnam-js-unit.js",
+                :optimizations :whitespace}}
+    {:source-paths ["purnam-js/src" "demos/crafty"],
+     :id "crafty-demo",
+     :compiler {:pretty-print true,
+                :output-to "harness/app/scripts/crafty-demo.js",
+                :optimizations :whitespace}}
     {:source-paths ["purnam-js/src" "purnam-angular/src" "demos/angular"],
      :id "angular-demo",
-     :compiler
-     {:pretty-print true,
-      :output-to "harness/app/scripts/angular-demo.js",
-      :optimizations :whitespace}}]})
+     :compiler {:pretty-print true,
+                :output-to "harness/app/scripts/angular-demo.js",
+                :optimizations :whitespace}}]})
