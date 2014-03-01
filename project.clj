80a25192111630d897bb1616e3e0f2b064cd932d
Chris Zheng <chris.zheng@auspost.com.au>
2014-03-01 18:13:25 +1100

    first commit

---
+++ project.clj
@@ -0,0 +1,18 @@
+(defproject im.chit/purnam "0.4.3"
+  :description "Language extensions for clojurescript"  
+  :url "http://www.github.com/purnam/purnam"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licencses/MIT"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [im.chit/purnam.native "0.4.3"]
+                 [im.chit/purnam.core   "0.4.3"]
+                 [im.chit/purnam.test   "0.4.3"]]
+  :test-paths ["test/clj"]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2138"]
+                                  [midje "1.6.0"]]
+                   :plugins [[lein-cljsbuild "1.0.0"]
+                             [lein-midje "3.1.3"]]}}
+  :cljsbuild {:builds [{:source-paths ["src"]
+                        :compiler {:output-to "target/purnam-test.js"
+                                   :optimizations :whitespace
+                                   :pretty-print true}}]})
