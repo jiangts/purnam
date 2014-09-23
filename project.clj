d8f2162348f3f2c17f2f5339eea86fbd6b7e67e2
Pedro Viegas <pedroasv@gmail.com>
2014-09-23 11:02:15 -0300

    updating dependencies

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
-(defproject im.chit/purnam "0.4.3"
+(defproject im.chit/purnam "0.4.4"
   :description "Language extensions for clojurescript"  
   :url "http://www.github.com/purnam/purnam"
   :license {:name "The MIT License"
             :url "http://opensource.org/licencses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [im.chit/purnam.native "0.4.3"]
-                 [im.chit/purnam.core   "0.4.3"]
-                 [im.chit/purnam.test   "0.4.3"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [im.chit/purnam.native "0.4.4"]
+                 [im.chit/purnam.core   "0.4.4"]
+                 [im.chit/purnam.test   "0.4.4"]]
   :test-paths ["test/clj"]
-  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2138"]
-                                  [midje "1.6.0"]]
-                   :plugins [[lein-cljsbuild "1.0.0"]
+  :profiles {:dev {:dependencies [[org.clojure/clojurescript "0.0-2342"]
+                                  [midje "1.6.3"]]
+                   :plugins [[lein-ancient "0.5.5"]
+                             [lein-cljsbuild "1.0.3"]
                              [lein-midje "3.1.3"]]}}
   :documentation {:files {"doc/index"
                         {:input "test/midje_doc/purnam_guide.clj"
